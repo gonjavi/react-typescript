9bd77e30a4193fed343135f269bc3c2fa06d9fa1
gonjavi <javiercaliescali@gmail.com>
2021-10-05 11:42:37 -0500

    basic types

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,19 @@ class App extends Component {
     // let firstValue: number = 8;
     // let firstValue: boolean = false;
     // let firstValue: number[]= [2, 3, 4];
-    let firstValue: Array<string> = ['3', 'hi', 'dos'];
+    // let firstValue: Array<string> = ['3', 'hi', 'dos'];
+
+    // tuple
+    let aTuple: [string, number] = ['Manny', 34];
+
     return (
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         
           <p>
-            The value {firstValue} is of {typeof firstValue} type!
+            The value {aTuple[0]} is of {typeof aTuple[0]} type!
           </p>
         
         </header>
