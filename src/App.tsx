8e79d3a2053f44f88fc82e73f1b2e50b25d0b693
gonjavi <javiercaliescali@gmail.com>
2021-10-05 11:37:12 -0500

    basic types

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    // let firstValue: string = 'Many';
+    // let firstValue: number = 8;
+    // let firstValue: boolean = false;
+    // let firstValue: number[]= [2, 3, 4];
+    let firstValue: Array<string> = ['3', 'hi', 'dos'];
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        
+          <p>
+            The value {firstValue} is of {typeof firstValue} type!
+          </p>
+        
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
